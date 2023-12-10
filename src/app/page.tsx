241ba850800601625911df0f93f66a9bc68262bf
Hítalo Rodrigues <hitalo005@gmail.com>
2023-12-10 19:38:26 -0300

    feat: first style and CategoryList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,80 @@
+'use client'
+import { CategoriesList } from "@/components/constants/data"
+import styled from "styled-components"
+
+const Header = styled.header`
+  display: flex;
+  background: var(--primary-color);
+`
+
+const MainContent = styled.main`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  background-color: var(--primary-container);
+`
+
+const ContentDiv = styled.div`
+  display: flex;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+`
+
+const CategoriesDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0.5rem;
+  gap: 1rem;
+  flex-wrap: wrap;
+  color: white;
+  width: 1440px;
+  background-color: var(--on-primary-container);
+`
+
+const CategorieCard = styled.button`
+  display: flex;
+  padding: 1rem;
+  gap: 0.5rem;
+  background-color: ${props => props.color};
+  width: 25rem;
+`
+
+const handleRenderizeListCategories = () => {
+  return CategoriesList.map(category => {
+    return (
+      <CategorieCard key={category.id} color={category.backgroundColor} >
+        <div>
+          {category.id}
+        </div>
+        <div>
+          <div>
+            {category.name}
+          </div>
+          <div>
+            {category.problems}
+          </div>
+          <div>
+            {category.solutions}
+          </div>
+        </div>
+      </CategorieCard >
+    )
+  })
+}
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-cyan-50">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1>Hello World algorithms</h1>
-      </div>
-    </main>
+    <MainContent>
+      <Header>
+          Header
+      </Header>
+      <ContentDiv className="font-serif">
+        <CategoriesDiv>
+          {handleRenderizeListCategories()}
+        </CategoriesDiv>
+      </ContentDiv>
+    </MainContent>
   )
 }
---
+++ src/components/constants/data.ts
@@ -0,0 +1,67 @@
+
+
+export const CategoriesList = [
+  {
+    id: 1,
+    name: "BEGGINNER",
+    problems: "336 Problems",
+    solutions: "0 Problems",
+    backgroundColor: "#1abc9c"
+  },
+  {
+    id: 2,
+    name: "AD-HOC",
+    problems: "850 Problems",
+    solutions: "0 Problems",
+    backgroundColor: "#F3A01B"
+  },
+  {
+    id: 3,
+    name: "STRINGS",
+    problems: "148 Problems",
+    solutions: "0 Problems",
+    backgroundColor: "#52AF18"
+  },
+  {
+    id: 4,
+    name: "DATA STRUCTURES AND LIBRARIES",
+    problems: "179 Problems",
+    solutions: "0 Problems",
+    backgroundColor: "#E74D3E"
+  },
+  {
+    id: 5,
+    name: "MATHEMATICS",
+    problems: "269 Problems",
+    solutions: "0 Problems",
+    backgroundColor: "#8F8372"
+  },
+  {
+    id: 6,
+    name: "PARADIGMS",
+    problems: "215 Problems",
+    solutions: "0 Problems",
+    backgroundColor: "#9B59B6"
+  },
+  {
+    id: 7,
+    name: "GRAPH",
+    problems: "277 Problems",
+    solutions: "0 Problems",
+    backgroundColor: "#EF6EA3"
+  },
+  {
+    id: 8,
+    name: "COMPUTATIONAL GEOMETRY",
+    problems: "83 Problems",
+    solutions: "0 Problems",
+    backgroundColor: "#34495E"
+  },
+  {
+    id: 9,
+    name: "SQL",
+    problems: "50 Problems",
+    solutions: "0 Problems",
+    backgroundColor: "#1F78A2"
+  }
+]
