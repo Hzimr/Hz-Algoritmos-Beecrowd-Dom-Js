c22a26e0c8f29c037604dc91e4be658591e218e1
Hítalo Rodrigues <hitalo005@gmail.com>
2023-12-10 17:59:01 -0300

    feat: config initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-cyan-50">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <h1>Hello World algorithms</h1>
       </div>
